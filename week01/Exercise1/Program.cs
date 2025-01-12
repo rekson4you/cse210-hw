af1049f90ed045d6fcdee1c516068c3e0d449242
rekson4you <rekson4you@yahoo.com>
2025-01-12 02:29:32 +0100

    Added space

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -7,9 +7,11 @@ class Program
         //Ask the user for their first name
         Console.Write("What is your first name? ");
         string first = Console.ReadLine();
+
         //Ask the user for their last name
         Console.Write("What is your last name? ");
         string last = Console.ReadLine();
+        
         // Displaying the names
         Console.WriteLine($"Your name is {last}, {first}, {last}");
 
